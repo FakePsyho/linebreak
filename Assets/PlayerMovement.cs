9a577f9574c85191feb9091e9dbf800dfdbd0d87
FakePsyho <closedeyez@gmail.com>
2018-04-14 14:30:18 +0200

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float moveAcc = 0.1f;
+
+	void Start() {
+		
+	}
+
+    void FixedUpdate() {
+        if (Input.GetKey("a"))) {
+
+        }
+    }
+	
+	void Update() {
+		
+	}
+}
